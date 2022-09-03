c3b60992a5d8f458afbab8154c92d251ba362ca3
Shriraj Solanki <solankishriraj33@gmail.com>
2022-09-03 15:31:55 +0530

    day 3 complited

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:project1/pages/login_page.dart';
 
-import 'home_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(myapp());
-}
+} 
 
 class myapp extends StatelessWidget {
   const myapp({Key? key}) : super(key: key);
@@ -12,7 +13,19 @@ class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+     // home: HomePage(),//This Work when we donnot add route
+     
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.brown ),
+      darkTheme: ThemeData( 
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",//this is for seting home page as first screen
+      routes:{
+        "/": (context) => new LoginPage(),//here we can also use new key word
+        "/home": (context) => HomePage(),
+        "/login":(context) =>LoginPage() , 
+      } ,
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("First App"),
+        title: Padding(
+          padding: const EdgeInsets.all(90.0),//given by me
+          child: Text("First App"),
+        ),
       ),
       body: Center(
         child: Container(
